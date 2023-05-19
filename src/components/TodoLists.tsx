321fc0d273171ed64bbe33bed2a11811fd7e1ab1
filipInSpace <121701664+filipInSpace@users.noreply.github.com>
2023-05-19 16:58:21 +0200

    Update TodoLists.tsx
    Styling components with TailwindCSS

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -1,4 +1,5 @@
 
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
@@ -64,8 +65,8 @@ const TodoLists: React.FC = () => {
       });
     reset();
   });
-    
-   const handleDeleteTodoList = (todoListId: number) => {
+
+  const handleDeleteTodoList = (todoListId: number) => {
     axios
       .get<ITodoItem[]>(`https://6425ff1f556bad2a5b47e151.mockapi.io/todo-lists/${todoListId}/items`)
       .then((response) => {
@@ -83,18 +84,17 @@ const TodoLists: React.FC = () => {
       });
   };
 
-
   return (
-    <div>
-      <h1>Todo Lists</h1>
-      <div>
+    <div className="bg-gray-100 py-6 px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Todo Lists</h1>
+      <div className="flex flex-wrap justify-center">
         {todoLists.map((todoList) => (
-          <div key={todoList.id}>
-            <div>
-              <h2>{todoList.name}</h2>
-              <button onClick={() => handleDeleteTodoList(todoList.id)}>Delete</button>
+          <div className="bg-white rounded-lg shadow-md p-6 mb-6 mr-6" key={todoList.id}>
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-lg font-bold text-gray-800 todo-list-name">{todoList.name}</h2>
+              <button className="bg-red-600 text-white rounded-lg px-4 py-2 hover:bg-red-700 focus:outline-none" onClick={() => handleDeleteTodoList(todoList.id)}>Delete</button>
             </div>
-            <ul>
+            <ul className="mt-4 flex flex-col">
               {todoList.items.map((item) => (
                 <TodoItem todoListId={todoList.id} key={item.id} />
               ))}
@@ -103,14 +103,15 @@ const TodoLists: React.FC = () => {
           </div>
         ))}
       </div>
-      <form onSubmit={handleAddTodoList}>
+      <form onSubmit={handleAddTodoList} className="mt-6 flex flex-col sm:flex-row">
         <input
+          className="rounded-lg border-gray-400 border-2 p-2 w-full sm:w-auto mb-2 sm:mb-0 sm:mr-2 focus:outline-none focus:border-primary"
           type="text"
           {...register("name")}
           placeholder="To-do List Name"
         />
-        <button type="submit">Add Todo List</button>
-        <p>{error}</p>
+        <button className="bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-400 focus:outline-none" type="submit">Add Todo List</button>
+        <p className="text-red-600 mt-2">{error}</p>
       </form>
     </div>
 );
