ad2eb16547515a6969ae3480bf3ad5dfe99f13e6
filipInSpace <121701664+filipInSpace@users.noreply.github.com>
2023-05-19 16:36:42 +0200

    Update App.tsx
    Routing Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TodoLists from "./components/TodoLists";
-
+import TodoItemDetails from "./components/TodoItemDetails";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TodoLists/>} />
+        <Route path="/todo-lists/:todoListId/items/:id" element={<TodoItemDetails/>} />
       </Routes>
     </Router>
   );
